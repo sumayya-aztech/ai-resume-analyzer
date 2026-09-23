e66cba70a29340d6fccafdb78b49221b1f9d8ea5
Sumayya <umithayyil5@gmail.com>
2026-09-23 12:46:57 +0400

    Cloud storage, PDF-Image conversion, Key-value storage, feedback

--- app/routes.ts
+++ app/routes.ts
@@ -3,4 +3,5 @@ import { route } from '@react-router/dev/routes';
 export default [
   index('routes/home.tsx'),
   route('/auth', 'routes/auth.tsx'),
+  route('/upload', 'routes/upload.tsx'),
 ] satisfies RouteConfig;
