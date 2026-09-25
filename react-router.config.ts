87eefb90af91079a4f53d83942eed638383f6d29
Sumayya <umithayyil5@gmail.com>
2026-09-25 14:22:12 +0400

    tesr

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,6 @@ import type { Config } from '@react-router/dev/config';
 
 export default {
   // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
+  // Server-side render by default, to enable SPA mode  send set this to `false`
   ssr: false,
 } satisfies Config;
